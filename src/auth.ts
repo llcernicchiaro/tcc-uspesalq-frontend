528628730bccf880a84b06a65b2033859d4bd2ab
Lorenzo Cernicchiaro <lopescernicchiaro@gmail.com>
2025-04-13 14:18:58 -0300

    remove role logic

--- src/auth.ts
+++ src/auth.ts
@@ -13,31 +13,4 @@ declare module "next-auth" {
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account?.id_token) {
-        const jwtPayload = JSON.parse(
-          Buffer.from(account.id_token.split(".")[1], "base64").toString()
-        );
-        console.log("ID Token payload:", jwtPayload);
-        token.groups = (jwtPayload["cognito:groups"] as string[]) || [];
-      }
-      return token;
-    },
-
-    async session({ session, token }) {
-      session.user.id = token.sub || "";
-      const groups = (token.groups as string[]) || [];
-
-      if (groups.includes("admin")) {
-        session.user.role = "admin";
-      } else if (groups.includes("participant")) {
-        session.user.role = "participant";
-      } else {
-        session.user.role = "unknown";
-      }
-
-      return session;
-    },
-  },
 });
